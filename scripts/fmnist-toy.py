c27cb57cc43646ad0e18a208455c91e77c492130
David Elkind <djelkind@gmail.com>
2021-09-08 23:38:52 -0400

    progress++

--- scripts/fmnist-toy.py
+++ scripts/fmnist-toy.py
@@ -24,14 +24,50 @@ from torchvision.datasets import FashionMNIST
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-b", "--batch_size", type=int, help="how many samples in each training batch"
+        "-b",
+        "--batch_size",
+        default=64,
+        type=int,
+        required=True,
+        help="how many samples in each training batch",
     )
     return parser.parse_args()
 
 
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 1),
+            # nn.BatchNorm2d(32),
+            nn.ELU(),
+            nn.Conv2d(32, 64, 3, 1),
+            # nn.BatchNorm2d(64),
+            nn.ELU(),
+            nn.MaxPool2d(2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(9216, 128),
+            # nn.BatchNorm1d(128),
+            nn.ELU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        x_cnn = self.conv(x)
+        x_flat = x_cnn.flatten(1)
+        x_logit = self.fc(x_flat)
+        return x_logit
+
+    def logsumexp_logits(self, x):
+        logits = self.forward(x)
+        return torch.logsumexp(logits, dim=1)
+
+
 class SgldHarness(object):
     def __init__(
-        self, net: nn.Module, replay_buffer, sgld_lr=1, sgld_step=20, noise=0.01
+        self, net: nn.Module, replay_buffer, sgld_lr=1., sgld_step=20, noise=0.01
     ):
         assert isinstance(replay_buffer, ReplayBuffer)
         assert isinstance(sgld_lr, float) and sgld_lr > 0.0
@@ -44,23 +80,23 @@ class SgldHarness(object):
         self.replay = replay_buffer
         self.sgld_optim = SGD(lr=sgld_lr, params=self.net.parameters())
 
-    def step(self, x_hat, y):
-        # TODO - do we need to know y to compute this? Need to review the paper... notation is confusing
-        x_hat.requires_grad = True
+    def step(self, x_hat):
         # TODO - get gradient for x wrt loss
         # https://discuss.pytorch.org/t/newbie-getting-the-gradient-with-respect-to-the-input/12709/7
-        x_grad = torch.autograd.grad(self.net(x_hat), x_hat, create_graph=True)
+        energy = self.net.logsumexp_logits(x_hat)
+        x_grad = torch.autograd.grad(energy, x_hat, create_graph=True)[0]
         gradient_step = self.sgld_lr * x_grad
         noise = self.noise * torch.randn(self.replay.shape)
         x_hat = x_hat + gradient_step + noise
-        x_hat[x_hat < self.replay.range[0]] = self.replay.range[0]
-        x_hat[x_hat > self.replay.range[1]] = self.replay.range[1]
+        x_hat[x_hat < min(self.replay.range)] = min(self.replay.range)
+        x_hat[x_hat > max(self.replay.range)] = max(self.replay.range)
         return x_hat
 
     def __call__(self):
         x_hat = self.replay.sample()
-        for _ in range(self.sgld_step):
-            x_hat = self.step(x_hat, y)
+        x_hat.requires_grad = True
+        for sgld_step_num in range(self.sgld_step):
+            x_hat = self.step(x_hat)
         self.replay.append(x_hat)
         return x_hat
 
@@ -78,7 +114,7 @@ class ReplayBuffer(object):
         assert isinstance(maxlen, int) and maxlen > 0
         assert 0.0 < prob_reinitialize < 1.0
 
-        self.range = sorted(data_range)
+        self.range = data_range
         self.shape = data_shape
         self.buffer = []
         self.prob_reinitialize = prob_reinitialize
@@ -94,46 +130,29 @@ class ReplayBuffer(object):
 
     def sample(self):
         if 0 < len(self.buffer) and np.random.rand() < 1.0 - self.prob_reinitialize:
-            i = np.random.choice(range(len(self.buffer)))
-            return self.buffer[i]
+            ndx = np.random.choice(range(len(self.buffer)))
+            return self.buffer[ndx]
         else:
-            return TorchUnif(self.range[0], self.range[1]).sample(*self.shape)
+            return TorchUnif(min(self.range), max(self.range)).sample(self.shape)
 
-
-class AugmentedFashionMnistDataset(Dataset):
-    def __init__(self, x, y, transform=None):
-        self.x = x
-        self.y = y
-        self.transform = transform
-
-    def __getitem__(self, item):
-        x = self.x[item]
-        if self.transform is not None:
-            x = self.transform(x)
-        return x, self.y[item]
-
-    def __len__(self):
-        return self.y.numel()
 
 if __name__ == "__main__":
     user_args = parse_args()
     dest_dir = pathlib.Path("local_data")
-    scale_xform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    scale_list = [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    scale_xform = transforms.Compose(scale_list)
+    augmentation_xform = transforms.Compose(
+        [transforms.RandomHorizontalFlip(p=0.5), transforms.RandomVerticalFlip(p=0.5)]
+        + scale_list
     )
     fashion_train = FashionMNIST(
         dest_dir,
         train=True,
-        transform=scale_xform,
+        transform=augmentation_xform,
         target_transform=None,
         download=True,
     )
-    augmentation_xform = transforms.Compose(
-        [transforms.RandomHorizontalFlip(p=0.5), transforms.RandomVerticalFlip(p=0.5)]
-    )
-    fashion_train = AugmentedFashionMnistDataset(
-        x=fashion_train.data, y=fashion_train.targets, transform=augmentation_xform
-    )
+    # TODO - create a transform for adding 0-mean noise with a chosen stddev
     fashion_train = DataLoader(
         fashion_train, batch_size=user_args.batch_size, shuffle=True
     )
@@ -145,7 +164,21 @@ if __name__ == "__main__":
         target_transform=None,
         download=True,
     )
-    fashion_test = AugmentedFashionMnistDataset(
-        x=fashion_test.data, y=fashion_test.targets, transform=None
-    )
     fashion_test = DataLoader(fashion_test, batch_size=1000, shuffle=True)
+
+    my_net = SimpleNet()
+    param_ct = sum(p.numel() for p in my_net.parameters() if p.requires_grad)
+    # print(my_net)
+    print(f"The network has {param_ct:,} parameters.")
+
+    my_buffer = ReplayBuffer(data_shape=(1,1,28,28), data_range=(-1,1))
+    my_sgld = SgldHarness(net=my_net,replay_buffer=my_buffer)
+
+    for i, (x, y) in enumerate(fashion_train):
+        y_logit = my_net(x)
+        print(y_logit)
+        nrg = my_net.logsumexp_logits(x)
+        print(nrg)
+        my_sgld()
+        break
+        # TODO asdf
